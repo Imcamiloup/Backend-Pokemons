c19f17b532456d09d7cde809f2b2a885d8fce0d1
Luis Gómez <118867262+Imcamiloup@users.noreply.github.com>
2024-04-12 07:02:37 -0500

    cors last set

--- api/src/app.js
+++ api/src/app.js
@@ -5,7 +5,6 @@ const express = require("express");
 const router = require("./routes");
 const morgan = require("morgan");
 const cors = require("cors");
-const { createProxyMiddleware } = require('http-proxy-middleware');
 
 const server = express();
 
@@ -14,26 +13,8 @@ server.use(morgan("dev")); //midleware get
 server.use(express.json()); //midleware post
 server.use(cors());
 
-// Configuración del proxy inverso
-server.use('/api', createProxyMiddleware({
-  target: 'https://backend-pokemons-production.up.railway.app',
-  changeOrigin: true,
-  pathRewrite: {
-    '^/api': '/pokemons', // Redirige las solicitudes a /api/pokemons al servidor backend
-  },
-  // Opciones de CORS si es necesario
-}));
-
+//Use the router for manage the routes
 server.use(router);
-
-server.get("/types", (req, res) => {
-  res.header("Access-Control-Allow-Origin", "http://localhost:5173"); // Permitir solicitudes desde tu frontend
-  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
-  
-  // Aquí deberías devolver los datos que correspondan a la ruta '/types'
-  // Por ejemplo:
-  res.json({ types: ["fire", "water", "grass"] });
-});
 
 //here verify the server is running
 server.get('/', (req, res) => {
