ffe5718de72d522b2773e3c011f61568bbbe4685
Luis Gómez <118867262+Imcamiloup@users.noreply.github.com>
2024-04-12 06:44:38 -0500

    cors last set

--- api/src/app.js
+++ api/src/app.js
@@ -5,20 +5,31 @@ const express = require("express");
 const router = require("./routes");
 const morgan = require("morgan");
 const cors = require("cors");
+const { createProxyMiddleware } = require('http-proxy-middleware');
 
 const server = express();
 
 
 server.use(morgan("dev")); //midleware get
 server.use(express.json()); //midleware post
-
 server.use(cors());
+
+// Configuración del proxy inverso
+server.use('/api', createProxyMiddleware({
+  target: 'https://backend-pokemons-production.up.railway.app',
+  changeOrigin: true,
+  pathRewrite: {
+    '^/api': '/pokemons', // Redirige las solicitudes a /api/pokemons al servidor backend
+  },
+  // Opciones de CORS si es necesario
+}));
 
 server.use(router);
 
 server.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Origin", "http://localhost:5173");
   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  next();
 });
 
 //here verify the server is running
